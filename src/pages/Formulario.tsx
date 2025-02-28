59fcc370615364fc9678753a170fb4c4d7042531
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2025-02-28 15:26:22 -0300

    feat: Reupdate

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -68,6 +68,7 @@ function Formulario() {
 
   return (
     <div className="min-h-screen relative overflow-hidden bg-[#0F0B21] flex flex-col items-center justify-between px-4 py-10">
+      {/* Animated background elements */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute -inset-[10px] opacity-50">
           <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] rounded-full bg-purple-600 blur-[120px]"></div>
