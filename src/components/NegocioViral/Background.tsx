78a54657196e50e1810511b66eae1027f49148c8
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2025-03-27 18:41:12 -0300

    feat: Update

--- src/components/NegocioViral/Background.tsx
+++ src/components/NegocioViral/Background.tsx
@@ -10,7 +10,7 @@ const Background: React.FC<BackgroundProps> = ({
   mobileImageUrl,
 }) => {
   return (
-    <div className="absolute mt-24 inset-0 z-0 h-[100vh] md:h-[90vh] lg:h-[85vh]">
+    <div className="absolute mt-24 inset-0 z-0 h-[100vh] md:h-[90vh] lg:h-[90vh]">
       <div
         className="absolute inset-0 bg-cover bg-top hidden md:block"
         style={{ backgroundImage: `url(${imageUrl})`, opacity: 1 }}
